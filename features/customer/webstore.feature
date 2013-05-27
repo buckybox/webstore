6d231a4144b7a472a652f2dea6f8f32b64afac64
Cédric Félizard <cedric@felizard.fr>
2013-05-27 14:24:35 +0000

    Cukes for webstore checkout + various webstore improvements
    Sorry for the master commit :/

---
+++ features/customer/webstore.feature
@@ -0,0 +1,40 @@
+@javascript
+Feature: Customer places an order
+  In order to buy veggies from a distributor
+  As an unauthenticated visitor
+  I want to be able to order via the webstore
+
+Background:
+  Given A distributor is in the system
+  And I am unauthenticated
+  # TODO test both:
+  # And I am a customer
+  # And I log in
+
+Scenario: Select a box
+  Given I am on the webstore
+  When I select a customisable box to order
+  Then I should be asked to customise the box
+
+Scenario: Customise a box
+  Given I am asked to customise the box
+  When I customise the box
+  Then I should be asked to log in or sign up
+
+Scenario: Choose my delivery frequency
+  Given I am asked to select my delivery frequency
+  When I select a monthly delivery frequency
+  Then I should be asked for my delivery address
+
+Scenario: Order a box
+  Given I am asked for my delivery address
+  When I select the payment option "Cash on delivery"
+  And I fill in my delivery address
+  Then My order should be placed
+  And I should see the details of my order
+
+Scenario: Review my box
+  Given I have just ordered a box
+  When I am viewing my dashboard
+  Then I should have an order
+
---
+++ features/step_definitions/for_customers/webstore_steps.rb
@@ -0,0 +1,115 @@
+Given /^I am on the webstore$/ do
+  path = webstore_store_path(distributor_parameter_name: @customer.distributor.parameter_name)
+  visit path
+end
+
+When /^I select a customisable box to order$/ do
+  find("#webstore-items").first(:button, "Order").click # first button found, e.g. first box
+end
+
+Then /^I should be asked to customise the box$/ do
+  step "I should be viewing the customise step"
+  step "I should not see a message"
+end
+
+Given /^I am asked to customise the box$/ do
+  steps %Q{
+    Given I am on the webstore
+    When I select a customisable box to order
+  }
+
+  step "I should be asked to customise the box"
+end
+
+When /^I customise the box$/ do
+  check "Customise my box"
+  select2_select("Advocado", from: :webstore_order_customise_dislikes_input)
+  # TODO test subs
+  click_button "Next"
+end
+
+Then "I should be asked to log in or sign up" do
+  step "I should be viewing the login step"
+  step "I should not see a message"
+end
+
+When /^I fill in my email address$/ do
+  fill_in :webstore_order_user_email, with: "starving.rabbit+#{SecureRandom.uuid}@example.net"
+  click_button "Next"
+end
+
+Given "I am asked to select my delivery frequency" do
+  steps %Q{
+    Given I am asked to customise the box
+    When I customise the box
+    When I fill in my email address
+  }
+end
+
+When /^I select a (.*) delivery frequency$/ do |frequency|
+  select frequency, from: :webstore_order_schedule_rule_frequency
+  click_button "Next"
+end
+
+Then /^I should be asked for my delivery address$/ do
+  step "I should be viewing the complete step"
+  step "I should not see a message"
+end
+
+Given "I am asked for my delivery address" do
+  steps %Q{
+    Given I am asked to select my delivery frequency
+    When I select a monthly delivery frequency
+  }
+
+  step "I should be asked for my delivery address"
+end
+
+When /^I fill in my delivery address$/ do
+  fill_in :webstore_order_address_name, with: "Crazy Rabbit"
+  fill_in :webstore_order_address_street_address, with: "Rabbit Hole"
+  click_button "Complete Order"
+end
+
+When /^I select the payment option "(.*)"$/ do |option|
+  within "#payment-options" do
+    choose option
+  end
+end
+
+Then /^My order should be placed$/ do
+  step 'I should see a success message with "Your order has been placed"'
+end
+
+Then /^I should see the details of my order$/ do
+  page.should have_content "Payment instructions"
+  # TODO more checks
+end
+
+Given "I have just ordered a box" do
+  steps %Q{
+    Given I am asked for my delivery address
+    When I select the payment option "Cash on delivery"
+    And I fill in my delivery address
+  }
+end
+
+Given /^I am viewing the (.*) step$/ do |step|
+  visit webstore_step_path(step)
+
+  step "I should be viewing the #{step} step"
+end
+
+Then /^I should be viewing the (.*) step$/ do |step|
+  expected_path = webstore_step_path(step)
+  current_path.should eq expected_path
+
+  page.should have_title "Bucky Box - Webstore - #{step.capitalize}"
+  step "I should not see an failure message"
+end
+
+def webstore_step_path step
+  path_helper = "webstore_#{step}_path"
+  public_send(path_helper, distributor_parameter_name: @customer.distributor.parameter_name)
+end
+
