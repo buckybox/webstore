bcac9c64071a15f2ba3ed2539103da53c0bc3323
Samson Ootoovak <sam@enspiral.com>
2013-06-17 12:28:04 +1200

    Get the customise step rendering with fake data.

--- app/models/webstore/customise.rb
+++ app/models/webstore/customise.rb
@@ -1,6 +1,46 @@
+require 'active_attr'
 require_relative '../webstore'
 
 class Webstore::Customise
+  include ActiveAttr::BasicModel
+
   def initialize(args = {})
+    @order = args[:order]
+  end
+
+  def dislikes?
+    true
+  end
+
+  def likes?
+    true
+  end
+
+  def extras_allowed?
+    true
+  end
+
+  def extras_unlimited?
+    false
+  end
+
+  def exclusions_limit
+    3
+  end
+
+  def substitutions_limit
+    3
+  end
+
+  def extras_limit
+    3
+  end
+
+  def stock_list
+    ['Apples', 'Banannas', 'Oranges', 'Grapes', 'Eggs', 'Coffee']
+  end
+
+  def extras
+    Extra.limit(5)
   end
 end
