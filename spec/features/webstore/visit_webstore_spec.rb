0ce1eff0c7b6a9a626333af586e1a7e9971fb7c4
Samson Ootoovak <sam@enspiral.com>
2013-07-26 13:53:56 +1200

    Pull out common test set up into a module.

--- spec/features/webstore/visit_webstore_spec.rb
+++ spec/features/webstore/visit_webstore_spec.rb
@@ -1,49 +1,9 @@
-require_relative 'webstore_helper'
+require_relative '../../support/webstore/webstore_helper'
 
 describe 'visit the webstore homepage' do
-  let(:distributor_name)           { 'Local Veg' }
-  let(:distributor_city)           { 'Wellington' }
-  let(:distributor_about)          { 'Local farm.' }
-  let(:distributor_details)        { 'Sells veg.' }
-  let(:distributor_facebook_url)   { 'http://fb.com' }
-  let(:product_name)               { 'Fruit Box' }
-  let(:product_description)        { 'Fruit in a box.' }
-  let(:product_price)              { 2.00 }
-  let(:hidden_product_name)        { 'Veg Box' }
-  let(:hidden_product_description) { 'Veg in a box.' }
-  let(:hidden_product_price)       { 3.00 }
-  let(:distributor) do
-    Fabricate(:distributor,
-      name:          distributor_name,
-      city:          distributor_city,
-      about:         distributor_about,
-      details:       distributor_details,
-      facebook_url:  distributor_facebook_url
-    )
-  end
-  let(:product) do
-    Fabricate(:box,
-      distributor:  distributor,
-      name:         product_name,
-      description:  product_description,
-      price:        product_price
-    )
-  end
-  let(:hidden_product) do
-    Fabricate(:box,
-      hidden:       true,
-      distributor:  distributor,
-      name:         hidden_product_name,
-      description:  hidden_product_description,
-      price:        hidden_product_price
-    )
-  end
+  include Webstore::StoreHelpers
 
-  before do
-    @distributor = distributor
-    setup_a_webstore(product)
-    visit webstore_store_path(distributor.parameter_name)
-  end
+  before { make_and_visit_a_webstore_with_products }
 
   it 'shows the distributor information' do
     expect(page).to have_content(distributor_name)
---
+++ spec/support/webstore/webstore_store_helpers.rb
@@ -0,0 +1,52 @@
+module Webstore::StoreHelpers
+  def self.included(base)
+    base.let(:distributor_name)           { 'Local Veg' }
+    base.let(:distributor_city)           { 'Wellington' }
+    base.let(:distributor_about)          { 'Local farm.' }
+    base.let(:distributor_details)        { 'Sells veg.' }
+    base.let(:distributor_facebook_url)   { 'http://fb.com' }
+
+    base.let(:distributor) do
+      Fabricate(:distributor,
+        name:          distributor_name,
+        city:          distributor_city,
+        about:         distributor_about,
+        details:       distributor_details,
+        facebook_url:  distributor_facebook_url
+      )
+    end
+
+    base.let(:product_name)               { 'Fruit Box' }
+    base.let(:product_description)        { 'Fruit in a box.' }
+    base.let(:product_price)              { 2.00 }
+
+    base.let(:product) do
+      Fabricate(:box,
+        distributor:  distributor,
+        name:         product_name,
+        description:  product_description,
+        price:        product_price
+      )
+    end
+
+    base.let(:hidden_product_name)        { 'Veg Box' }
+    base.let(:hidden_product_description) { 'Veg in a box.' }
+    base.let(:hidden_product_price)       { 3.00 }
+
+    base.let(:hidden_product) do
+      Fabricate(:box,
+        hidden:       true,
+        distributor:  distributor,
+        name:         hidden_product_name,
+        description:  hidden_product_description,
+        price:        hidden_product_price
+      )
+    end
+  end
+
+  def make_and_visit_a_webstore_with_products
+    @distributor = distributor
+    setup_a_webstore(product) # Send at least one product so it doesn' make it's own
+    visit webstore_store_path(distributor.parameter_name)
+  end
+end
