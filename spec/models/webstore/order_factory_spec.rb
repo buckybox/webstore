d46a6ab1d34f5209a95224af53e5d34643f384d0
Cédric Félizard <cedric@felizard.fr>
2013-07-29 03:27:23 +0000

    Add some more tests for OrderFactory

--- spec/models/webstore/order_factory_spec.rb
+++ spec/models/webstore/order_factory_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Webstore::OrderFactory do
+  let(:route) { Fabricate(:route) }
+
   # stub data from `Webstore::CartPersistance.last.collected_data.order`
   let(:information) do
     {
@@ -9,10 +11,10 @@ describe Webstore::OrderFactory do
       extras: { 156 => 2 },
       email: nil, # FIXME seems we don't capture the email?
       password: nil,
-      route_id: 177,
+      route_id: route.id,
       start_date: "Fri, 02 Aug 2013",
       frequency: "weekly",
-      days: {5 => 1},
+      days: {2 => 1, 5 => 1},
       extra_frequency: false,
       name: nil,
       phone_number: nil,
@@ -56,8 +58,19 @@ describe Webstore::OrderFactory do
       @new_order = @factory.order
     end
 
+    specify { expect(@new_order.completed).to be_true }
+    specify { expect(@new_order.active).to be_true }
+
     it "has the right quantity" do
       expect(@new_order.quantity).to eq(Order::QUANTITY)
+    end
+
+    it "has the right box" do
+      expect(@new_order.box).to eq(box)
+    end
+
+    xit "has the right route" do
+      expect(@new_order.route).to eq(route)
     end
 
     it "has the extras" do
@@ -80,8 +93,23 @@ describe Webstore::OrderFactory do
       end
 
       it "has the right days" do
-        expect(schedule_rule.days).to eq([:fri]) # FIXME
+        expect(schedule_rule.days).to eq([:tue, :fri])
       end
     end
+
+    context "customer" do
+      it "has the right customer" do
+        expect(@new_order.customer).to eq(customer)
+      end
+
+      it "has the right account" do
+        expect(@new_order.account).to eq(customer.account)
+      end
+
+      it "has the right address" do
+        expect(@new_order.address).to eq(customer.address)
+      end
+    end
+
   end
 end
