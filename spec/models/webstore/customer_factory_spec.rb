4ff9501b5d99e99e283ab52cf528592c3dfbac5d
Cédric Félizard <cedric@felizard.fr>
2013-07-31 06:51:54 +0000

    Add some more tests for OrderFactory

--- spec/models/webstore/customer_factory_spec.rb
+++ spec/models/webstore/customer_factory_spec.rb
@@ -1,17 +1,49 @@
 require 'spec_helper'
 
 describe Webstore::CustomerFactory do
-    let(:customer) do
-      {
-        customer: nil,
-        distributor: nil,
-      }
+  let(:distributor) { Fabricate(:distributor) }
+
+  let(:customer) do
+    {
+      customer: nil,
+      distributor: distributor,
+    }
+  end
+
+  let(:box) { Fabricate(:customisable_box) }
+  let(:order) do
+    Webstore::Order.new(
+      box: box,
+      cart: nil,
+      information: information_hash,
+    )
+  end
+
+  let(:args) { { cart: nil, customer: customer } }
+
+  describe "#initialize" do
+    it "accepts a cart and a customer" do
+      Webstore::OrderFactory.new args
+    end
+  end
+
+  describe "#assemble" do
+    before do
+      @factory = Webstore::CustomerFactory.new(args)
+      @new_customer = @factory.assemble
     end
 
-    let(:cart) { double("Webstore::Cart", customer: customer) }
-
-    it "accepts a cart" do
-      Webstore::CustomerFactory.new cart
+    it "sets the right email" do
+      expect(@new_customer.email).to eq(customer.email)
     end
+
+    it "sets the right account" do
+      expect(@new_customer.account).to eq(customer.account)
+    end
+
+    it "sets the right address" do
+      expect(@new_customer.address).to eq(customer.address)
+    end
+  end
 end
 
--- spec/models/webstore/order_factory_spec.rb
+++ spec/models/webstore/order_factory_spec.rb
@@ -1,48 +1,58 @@
 require 'spec_helper'
 
 describe Webstore::OrderFactory do
-  let(:route) { Fabricate(:route) }
-
-  # stub data from `Webstore::CartPersistance.last.collected_data.order`
-  let(:information) do
-    {
-      # dislikes: [646], # FIXME to test
-      # likes: [652], # FIXME to test
-      extras: { 156 => 2 },
-      email: nil, # FIXME seems we don't capture the email?
-      password: nil,
-      route_id: route.id,
-      start_date: "Fri, 02 Aug 2013",
-      frequency: "weekly",
-      days: {2 => 1, 5 => 1},
-      extra_frequency: false,
-      name: nil,
-      phone_number: nil,
-      phone_type: nil,
-      street_address: nil,
-      street_address_2: nil,
-      suburb: nil,
-      city: "Southwell",
-      postcode: nil,
-      delivery_note: nil,
-      payment_method: "bank_deposit",
-      complete: true
-    }
-  end
-
   let(:box) { Fabricate(:customisable_box) }
+  let(:exclusion) { Fabricate(:exclusion) }
+  let(:substitution) { Fabricate(:substitution) }
 
-  let(:order) do
-    Webstore::Order.new(
-      box: box,
-      cart: nil,
-      information: information
+  let!(:cart) do
+    Webstore::Cart.new(
+      customer: Webstore::Customer.new(
+        cart: nil,
+        customer: nil,
+        distributor: Distributor.new
+      ),
+      order: {
+        box: box,
+        cart: nil,
+        information: {
+          :dislikes=>[
+            exclusion.id
+          ],
+          :likes=>[
+            substitution.id
+          ],
+          :extras=> {
+            box.extras[0].id => 2,
+            box.extras[1].id => 3,
+          },
+          :email=>"test@example.net",
+          :password=>"",
+          :route_id=>1,
+          :start_date=>"Fri, 02 Aug 2013",
+          :frequency=>"weekly",
+          :days=>{2=>1, 5=>1},
+          :extra_frequency=>false,
+          :name=>"Bob",
+          :phone_number=>nil, # FIXME
+          :phone_type=>nil, # FIXME
+          :street_address=>"Street 1",
+          :street_address_2=>"",
+          :suburb=>"",
+          :city=>"Southwell",
+          :postcode=>"",
+          :delivery_note=>"",
+          :payment_method=>"bank_deposit",
+          :complete=>true
+        }
+      }
     )
   end
 
-  let(:cart) { double("Webstore::Cart", order: order) }
-  let(:customer) { Fabricate(:customer) }
+  let(:customer) { Fabricate.build(:customer) }
   let(:args) { { cart: cart, customer: customer } }
+
+  let!(:information_hash) { cart.order.information }
 
   describe "#initialize" do
     it "accepts a cart and a customer" do
@@ -50,66 +60,63 @@ describe Webstore::OrderFactory do
     end
   end
 
-  describe "#assemble" do
+  describe "#assemble", focus: true do
     before do
       @factory = Webstore::OrderFactory.new(args)
-      @factory.assemble
-
-      @new_order = @factory.order
+      @new_order = @factory.assemble
     end
 
     specify { expect(@new_order.completed).to be_true }
     specify { expect(@new_order.active).to be_true }
 
-    it "has the right quantity" do
+    it "sets the right quantity" do
       expect(@new_order.quantity).to eq(Order::QUANTITY)
     end
 
-    it "has the right box" do
+    it "sets the right box" do
       expect(@new_order.box).to eq(box)
     end
 
-    xit "has the right route" do
-      expect(@new_order.route).to eq(route)
+    xit "sets the right route" do
+      expect(@new_order.route.id).to eq(information_hash[:route_id])
     end
 
-    it "has the extras" do
-      information[:extras].each do |id, count|
-        expect(@new_order.order_extras.detect do |order_extra|
-          order_extra.extra_id == id && order_extra.count == count
-        end).to_not be_nil
+    it "sets the exclusions" do
+      expect(@new_order.exclusions).to eq([exclusion])
+    end
+
+    it "sets the substitutions" do
+      expect(@new_order.substitutions).to eq([substitution])
+    end
+
+    it "sets the extras" do
+      information_hash[:extras].each do |id, count|
+        order_extra = @new_order.order_extras.detect do |oe|
+          oe.extra_id == id && oe.count == count
+        end
+
+        expect(order_extra).to_not be_nil
       end
     end
 
     context "#schedule_rule" do
       let(:schedule_rule) { @new_order.schedule_rule }
 
-      it "has the right start_date" do
-        expect(schedule_rule.start.inspect).to eq(information[:start_date])
+      it "sets the right start_date" do
+        expect(schedule_rule.start.inspect).to eq(information_hash[:start_date])
       end
 
-      it "has the right frequency" do
-        expect(schedule_rule.frequency.to_s).to eq(information[:frequency])
+      it "sets the right frequency" do
+        expect(schedule_rule.frequency.to_s).to eq(information_hash[:frequency])
       end
 
-      it "has the right days" do
+      it "sets the right days" do
         expect(schedule_rule.days).to eq([:tue, :fri])
       end
     end
 
-    context "customer" do
-      it "has the right customer" do
-        expect(@new_order.customer).to eq(customer)
-      end
-
-      it "has the right account" do
-        expect(@new_order.account).to eq(customer.account)
-      end
-
-      it "has the right address" do
-        expect(@new_order.address).to eq(customer.address)
-      end
+    it "sets the right customer" do
+      expect(@new_order.customer).to eq(customer)
     end
-
   end
 end
