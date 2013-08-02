a7a0730cd177958b443513225a636bf1108e65d8
Cédric Félizard <cedric@felizard.fr>
2013-08-02 00:51:07 +0000

    Move function up so it is defined

--- app/assets/javascripts/old/webstore.js
+++ app/assets/javascripts/old/webstore.js
@@ -105,22 +105,16 @@ $(function() {
   }
 
   if($('#webstore-login').length > 0) {
+    function password_field_visibility() {
+      var show_password_field = ($('#registered input[type=radio]:checked').val() != 'new');
+      $('#password-field').toggle(show_password_field);
+    }
+
     password_field_visibility();
 
     $('#registered input[type="radio"]').click(function() {
       password_field_visibility();
     });
-
-    function password_field_visibility(value) {
-      var show_password_field = ($('#registered input[type=radio]:checked').val() == 'new');
-
-      if(show_password_field) {
-        $('#password-field').hide();
-      }
-      else {
-        $('#password-field').show();
-      }
-    }
   }
 
   if($('#webstore-route').length > 0) {
