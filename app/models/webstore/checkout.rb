081248318eeb459c21377758416b22721476d47e
Samson Ootoovak <sam@enspiral.com>
2013-08-05 14:38:07 +1200

    Add the existing customer to the new webstore cart on checkout.

--- app/models/webstore/checkout.rb
+++ app/models/webstore/checkout.rb
@@ -8,11 +8,11 @@ class Webstore::Checkout
     args               = defaults.merge(args)
     @distributor       = args[:distributor]
     @existing_customer = args[:existing_customer]
-    @cart              = args[:cart_class].new(distributor: distributor)
+    @cart              = args[:cart_class].new(distributor: distributor, customer: customer_hash)
   end
 
   def customer
-    @cart.customer
+    cart.customer
   end
 
   def add_product!(product_id)
@@ -28,6 +28,10 @@ private
 
   attr_reader :distributor
   attr_reader :existing_customer
+
+  def customer_hash
+    { existing_customer: existing_customer }
+  end
 
   def defaults
     { cart_class: Webstore::Cart }
