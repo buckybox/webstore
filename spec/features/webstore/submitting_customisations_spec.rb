f731b921d16a9646b0629d4ad76c600ac888f6e7
Cédric Félizard <cedric@felizard.fr>
2013-08-05 10:08:42 +0000

    Integration spec for customise step

--- spec/features/webstore/submitting_customisations_spec.rb
+++ spec/features/webstore/submitting_customisations_spec.rb
@@ -1,53 +1,73 @@
-require_relative '../../support/capybara/select2_helper'
-require_relative '../../support/webstore/webstore_helper'
+require_relative "../../support/capybara/select2_helper"
+require_relative "../../support/webstore/webstore_helper"
 
-describe 'submitting customisations' do
+describe "submitting customisations" do
   include Select2Helper
   include Webstore::StoreHelpers
 
-  before { get_to_customise_step }
+  before do
+    get_to_customise_step
+  end
 
-  describe 'only selected exclustions' do
+  context "with exclusions" do
     before do
-      check 'Customise my box'
-      select2 @line_items[0].name, from: 'webstore_customise_order_dislikes'
+      check "Customise my box"
+      select2 @dislike, from: "webstore_customise_order_dislikes"
     end
 
-    it 'shows the order so far' do
+    it "displays them" do
+      submit
+
+      page.should have_content @dislike
+    end
+
+    context "with substitutes" do
+      before do
+        select2 @like, from: "webstore_customise_order_likes"
+      end
+
+      it "displays them" do
+        submit
+
+        page.should have_content @like
+      end
     end
   end
 
-  describe 'selected exclustions and substitutions' do
-    it 'shows the order so far' do
+  context "with extras" do
+    before do
+      check "Customise my box"
+      select2 /^#{@extra}/, from: "webstore_customise_order_add_extra"
     end
 
-  end
+    it "displays them" do
+      submit
 
-  describe 'selected extras' do
-    it 'shows the order so far' do
+      page.should have_content @extra
     end
-
-  end
-
-  describe 'seleted exclustions, substitutions, and extras' do
-    it 'shows the order so far' do
-    end
-
   end
 
   def get_to_customise_step
     make_a_webstore_with_products
     visit webstore_store_path(@distributor.parameter_name)
     customisable_product(product)
-    click_button 'Order'
+    click_button "Order"
+  end
+
+  def submit
+    click_button "Next"
   end
 
   def customisable_product(product)
-    @extras = []
-    @line_items = []
-    2.times { @extras << Fabricate(:extra, distributor: @distributor) }
-    4.times { @line_items << Fabricate(:line_item, distributor: @distributor) }
-    @extras.each { |extra| Fabricate(:box_extra, extra: extra, box: product) }
+    @line_items = 2.times.map { Fabricate(:line_item, distributor: @distributor) }
+
+    @extras = [ Fabricate(:extra, distributor: @distributor) ]
+    Fabricate(:box_extra, extra: @extras[0], box: product)
+
+    @dislike = @line_items[0].name
+    @like = @line_items[1].name
+    @extra = @extras[0].name
+
     product.likes        = true
     product.dislikes     = true
     product.extras_limit = -1
