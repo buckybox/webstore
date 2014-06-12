2160861e34e68b10060a0839cf1cacd496299393
Cédric Félizard <cedric@felizard.fr>
2014-06-12 14:11:25 +0000

    Merge area_of_service and estimated_delivery_time into one field

--- app/decorators/webstore/delivery_service_decorator.rb
+++ app/decorators/webstore/delivery_service_decorator.rb
@@ -4,18 +4,8 @@ require_relative '../../models/delivery_service'
 class Webstore::DeliveryServiceDecorator < Draper::Decorator
   delegate_all
 
-  def area_of_service
-    h.simple_format(object.area_of_service)
-  end
-
-  def estimated_delivery_time
-    h.simple_format(object.estimated_delivery_time)
-  end
-
-  def description
-    result = object.area_of_service
-    result += "\n\n" + object.estimated_delivery_time
-    h.simple_format(result.html_safe)
+  def instructions
+    h.simple_format(object.instructions)
   end
 
   def schedule_input_id
