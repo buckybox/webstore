1b9c0d5309208a711510e4801ce1f5af472a98a2
Cédric Félizard <cedric@felizard.fr>
2014-08-29 03:41:00 +0000

    Disable ActiveRecord since we won't need it for now

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
 require "active_model/railtie"
-require "active_record/railtie"
+# require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
@@ -28,6 +28,6 @@ module Webstore
     # config.i18n.default_locale = :de
 
     # For not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
