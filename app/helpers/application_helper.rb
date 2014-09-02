f20270b207da9ff88bcc7b7fa0f058c758d6dbb2
Cédric Félizard <cedric@felizard.fr>
2014-09-02 10:45:31 +0000

    Render flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def render_flash_messages(flash)
+    flash.map do |type, message|
+      content_tag(:div, class: "alert #{alert_class_for(type)}") do
+        button_tag("×", class: "close", data: { dismiss: "alert" }) << message
+      end
+    end.join.html_safe
+  end
+
+  private def alert_class_for(flash_type)
+    {
+      success: 'alert-success',
+      error:   'alert-danger',
+      alert:   'alert-warning',
+      notice:  'alert-info'
+    }[flash_type.to_sym] || flash_type.to_s
+  end
 end
