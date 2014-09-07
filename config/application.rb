1d433aba577beef031b64e67ded9980aa2cbe037
Cédric Félizard <cedric@felizard.fr>
2014-09-07 15:33:51 +0000

    Move checkout models into their own directory

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,9 @@ module BuckyBox
 
       # For not swallow errors in after_commit/after_rollback callbacks.
       # config.active_record.raise_in_transactional_callbacks = true
+
+      # Custom directories with classes and modules you want to be autoloadable.
+      config.autoload_paths += Dir["#{config.root}/app/models/checkout"]
     end
   end
 end
