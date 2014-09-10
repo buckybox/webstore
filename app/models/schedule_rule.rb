8574a31d7d3349f0918a63bbdfa81563f1f3d320
Cédric Félizard <cedric@felizard.fr>
2014-09-10 14:20:23 +0000

    Implement ScheduleRule class

---
+++ app/models/schedule_rule.rb
@@ -0,0 +1,46 @@
+class ScheduleRule
+  attr_accessor :frequency, :start_date, :days
+
+  FREQUENCIES = %i(single weekly fortnightly monthly).freeze
+
+  def initialize(frequency:, start_date:, days:)
+    self.frequency = frequency.to_sym
+    raise ArgumentError unless FREQUENCIES.include?(self.frequency)
+
+    self.start_date = start_date.is_a?(Date) ? start_date : Date.parse(start_date)
+
+    self.days = days.keys.map(&:to_i)
+    raise ArgumentError if self.days.any? { |day| day < 0 || day > 27 }
+  end
+
+  def delivery_days
+    I18n.t('date.day_names').select.each_with_index do |_day, index|
+      runs_on index
+    end.to_sentence
+  end
+
+  def week_days
+    days.map { |day| day % 7 }
+  end
+
+  def week
+    (days.first / 7).floor
+  end
+
+  def runs_on(week_day_index)
+    week_days.include?(week_day_index)
+  end
+
+  def to_s
+    case frequency
+    when :single
+      "#{I18n.t('models.schedule_rule.deliver_on')} #{I18n.l(start_date, format: "%d %b")}"
+    when :weekly
+      "#{I18n.t('models.schedule_rule.deliver_weekly_on')} #{delivery_days}"
+    when :fortnightly
+      "#{I18n.t('models.schedule_rule.deliver_fornightly_on')} #{delivery_days}"
+    when :monthly
+      "#{I18n.t('models.schedule_rule.deliver_monthly_on')} #{week.succ.ordinalize_in_full} #{delivery_days}"
+    end
+  end
+end
---
+++ spec/models/schedule_rule_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+describe ScheduleRule do
+  let(:schedule) { ScheduleRule.new(frequency: :monthly, start_date: "2014-09-10", days: { 10 => 1, 13 => 1 }) }
+
+  before { I18n.locale = :en }
+
+  describe "#delivery_days" do
+    specify { expect(schedule.delivery_days).to eq "Wednesday and Saturday" }
+  end
+
+  describe "#to_s" do
+    specify { expect(schedule.to_s).to eq "Deliver monthly on the second Wednesday and Saturday" }
+  end
+end
