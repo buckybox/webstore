aad7f8b0385d25f286d3758e8ccc4f8df63b20d8
Cédric Félizard <cedric@felizard.fr>
2014-09-25 09:47:58 +0000

    Fail gracefully if config isn't present

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,4 @@
-if defined? Airbrake
+if defined?(Airbrake) && Figaro.env.airbrake_host.present? && Figaro.env.airbrake_api_key.present?
   Airbrake.configure do |config|
     config.host    = Figaro.env.airbrake_host
     config.api_key = Figaro.env.airbrake_api_key
--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,4 @@
-if defined? Bugsnag
+if defined?(Bugsnag) && Figaro.env.bugsnag_api_key.present?
   Bugsnag.configure do |config|
     config.api_key = Figaro.env.bugsnag_api_key
     config.use_ssl = true
