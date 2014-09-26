9b5b3929e583592ec47aff87f55b84d6433b747c
Cédric Félizard <cedric@felizard.fr>
2014-09-26 03:58:03 +0000

    Do it the Rails 4 way

--- config/initializers/i18n.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env.development? || Rails.env.test?
-  I18n.exception_handler = lambda do |exception, locale, key, options|
-    raise "Missing translation for locale #{locale}: #{key}"
-  end
-end
