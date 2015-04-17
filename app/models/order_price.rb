5ef66a768c6334dc217d76ba93c9d950d2cd2906
Cédric Félizard <cedric@felizard.fr>
2015-04-17 16:54:27 +0000

    Remove clutter from OrderPrice

--- app/models/order_price.rb
+++ app/models/order_price.rb
@@ -1,19 +1,16 @@
 class OrderPrice
-  def self.discounted(price, customer_discount = nil)
-    return price if customer_discount.nil? # Convenience so we don't have to check all over app for nil
+  def self.discounted(price, customer)
+    return price if customer.nil?
 
-    customer_discount = BigDecimal.new(customer_discount.discount.to_s) if customer_discount.respond_to?(:discount)
-
+    customer_discount = BigDecimal.new(customer.discount.to_s)
     price * (1 - customer_discount)
   end
 
-  def self.extras_price(order_extras, customer_discount = nil)
-    customer_discount = BigDecimal.new(customer_discount.discount.to_s) if customer_discount.respond_to?(:discount)
+  def self.extras_price(order_extras, customer)
+    total_price = order_extras.sum do |order_extra|
+      CrazyMoney.new(order_extra.fetch(:price)) * order_extra.fetch(:count)
+    end
 
-    total_price = order_extras.map do |order_extra|
-      CrazyMoney.new(order_extra[:price]) * order_extra[:count]
-    end.sum
-
-    discounted(total_price, customer_discount)
+    discounted(total_price, customer)
   end
 end
