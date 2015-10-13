a8f9208cf3753f88ed0eacd39271554021b72e32
Cédric Félizard <cedric@felizard.fr>
2015-10-13 17:41:33 +0000

    Enforce CSP

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -4,12 +4,13 @@ SecureHeaders::Configuration.configure do |config|
   config.x_frame_options = "DENY"
   config.x_xss_protection = { value: 1, mode: "block" }
   config.csp = {
-    enforce: false,
-    default_src: "'self'",
+    enforce: true,
+    default_src: "'none'",
     img_src: "'self' my.buckybox.com *.google-analytics.com *.pingdom.net",
-    script_src: "'self' 'unsafe-inline' www.google-analytics.com *.pingdom.net js-agent.newrelic.com bam.nr-data.net",
+    script_src: "'self' 'unsafe-inline' *.google-analytics.com *.pingdom.net js-agent.newrelic.com bam.nr-data.net",
     style_src: "'self' 'unsafe-inline'",
     form_action: "'self' www.paypal.com",
+    connect_src: "'self' *.google-analytics.com",
     frame_ancestors: "mydeal-tahiti.com",
     block_all_mixed_content: "",
     report_uri: "https://api.buckybox.com/v1/csp-report",
