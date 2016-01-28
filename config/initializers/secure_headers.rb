71aa2b49aa571cebe92dff114529fc1eb1c18b5f
Cédric Félizard <cedric@felizard.fr>
2016-01-28 21:37:36 +0000

    Update config for new version of SecureHeaders

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,18 +1,17 @@
 require "secure_headers"
 
-SecureHeaders::Configuration.configure do |config|
-  config.x_frame_options = false
-  config.x_xss_protection = { value: 1, mode: "block" }
+SecureHeaders::Configuration.default do |config|
+  config.x_frame_options = "DENY"
+  config.x_xss_protection = "1; mode=block"
   config.csp = {
     enforce: true,
-    default_src: "'none'",
-    img_src: "'self' my.buckybox.com *.google-analytics.com *.pingdom.net notify.bugsnag.com *.tile.openstreetmap.org",
-    script_src: "'self' 'unsafe-inline' *.google-analytics.com *.pingdom.net js-agent.newrelic.com bam.nr-data.net https://d2wy8f7a9ursnm.cloudfront.net/bugsnag-2.min.js",
-    style_src: "'self' 'unsafe-inline'",
-    form_action: "'self' www.paypal.com",
-    connect_src: "'self' api.buckybox.com *.google-analytics.com",
-    block_all_mixed_content: "",
-    report_uri: "https://api.buckybox.com/v1/csp-report",
+    default_src: %w('none'),
+    img_src: %w('self' my.buckybox.com *.google-analytics.com *.pingdom.net notify.bugsnag.com *.tile.openstreetmap.org),
+    script_src: %w('self' 'unsafe-inline' *.google-analytics.com *.pingdom.net js-agent.newrelic.com bam.nr-data.net https://d2wy8f7a9ursnm.cloudfront.net/bugsnag-2.min.js),
+    style_src: %w('self' 'unsafe-inline'),
+    form_action: %w('self' www.paypal.com),
+    connect_src: %w('self' api.buckybox.com *.google-analytics.com),
+    report_uri: %w(https://api.buckybox.com/v1/csp-report),
   }
   # config.hpkp = {
   # TODO: set up HPKP
