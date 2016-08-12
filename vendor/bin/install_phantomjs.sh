5fa572e2d4c2f0f282316bd596faa313c35b1933
Cédric Félizard <cedric@felizard.fr>
2016-08-12 15:17:53 +0000

    Use alternative URL to get PhantomJS since Bitbucket keeps hitting rate limiting
    https://github.com/ariya/phantomjs/issues/13953

--- vendor/bin/install_phantomjs.sh
+++ vendor/bin/install_phantomjs.sh
@@ -3,11 +3,10 @@
 # Install PhantomJS on a Debian-like distro.
 
 VERSION=2.1.1
-ARCH=$(uname -m)
-PHANTOM_JS="phantomjs-$VERSION-linux-$ARCH"
+PHANTOM_JS=phantomjs-$VERSION-linux-x86_64
 
 cd /tmp
-wget --no-verbose https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
-tar xjf $PHANTOM_JS.tar.bz2
+wget --no-verbose https://github.com/Medium/phantomjs/releases/download/v$VERSION/$PHANTOM_JS.tar.bz2
+tar xf $PHANTOM_JS.tar.bz2
 mv $PHANTOM_JS /usr/local/share
 ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
