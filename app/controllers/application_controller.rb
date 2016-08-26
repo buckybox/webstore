673f89b4436d88fb4f4faff842152687721d5279
Cédric Félizard <cedric@felizard.fr>
2016-08-26 23:12:18 +0000

    Ignore Pingdom health checks in NewRelic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :new_relic_ignore_pingdom
+
+private
+
+  def new_relic_ignore_pingdom
+    NewRelic::Agent.ignore_transaction if request.user_agent =~ /pingdom/i
+  end
 end
