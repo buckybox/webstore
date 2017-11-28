053fb55b59eb9882c3ac34d11d54775722643dac
Cédric Félizard <cedric@felizard.fr>
2017-11-28 11:20:37 +0000

    Fix config for Bugsnag v6
    https://github.com/bugsnag/bugsnag-ruby/blob/master/UPGRADING.md

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -4,7 +4,6 @@ if defined?(Bugsnag) && Figaro.env.bugsnag_api_key.present?
   # :nocov:
   Bugsnag.configure do |config|
     config.api_key = Figaro.env.bugsnag_api_key
-    config.use_ssl = true
     config.notify_release_stages = %w[production staging]
   end
   # :nocov:
